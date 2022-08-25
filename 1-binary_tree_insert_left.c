4f425a2db16a5edec1aae7705f08034a46607df1
Slamchillz <mendyslam@gmail.com>
2022-08-25 11:13:27 +0000

    Node insertion correction

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,11 +9,13 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *leftNode = binary_tree_node(parent, value);
+	binary_tree_t *leftNode;
 
-	if (parent == NULL || leftNode == NULL)
+	if (parent == NULL)
 		return (NULL);
-
+	leftNode = binary_tree_node(parent, value);
+	if (leftNode == NULL)
+		return (NULL);
 	if (parent->left)
 		leftNode->left = parent->left, parent->left->parent = leftNode;
 
