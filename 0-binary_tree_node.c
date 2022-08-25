1ea96bbc54641adb6189c4aba0cfb33adb31f5e9
Slamchillz <mendyslam@gmail.com>
2022-08-25 12:12:31 +0000

    Code revert

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,24 +1,24 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - create a tree node
- *
+ * binary_tree_node - creates a binary tree node
  * @parent: parent node
- * @value: data
- * Return: pointer to new node
+ * @value: node n data, integer
+ *
+ * Return: binary_tree_t node on SUCCESS or NULL on FAILURE
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *temp;
+	binary_tree_t *newNode;
 
-	temp = malloc(sizeof(binary_tree_t));
-	if (temp == NULL)
+	newNode = (binary_tree_t *) malloc(sizeof(binary_tree_t));
+
+	if (newNode == NULL)
 		return (NULL);
 
-	temp->n = value;
-	temp->left = NULL;
-	temp->right = NULL;
-	temp->parent = parent;
+	newNode->n = value, newNode->parent = parent;
+	newNode->left = NULL, newNode->right = NULL;
 
-	return (temp);
+	return (newNode);
 }
+
