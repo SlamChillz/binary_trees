3e638b3724e96dddaa7a9b5d2f16abcd698177e7
Slamchillz <mendyslam@gmail.com>
2022-08-27 16:12:46 +0000

    Smarter binary search tree insertion

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,6 +1,31 @@
 #include "binary_trees.h"
 
 /**
+ * _bst_insert - performs clean avl insert into an avl tree
+ * @node: pointer to the node to be inserted in
+ * @value: new value to be inserted
+ * @parent: pointer to the parent node of the newly created node
+ * @new: double pointer to store the new node
+ *
+ * Return: pointer to the root node of tree after insertion or NULL on failure
+ */
+bst_t *_bst_insert(bst_t *node, int value, bst_t *parent, bst_t **new)
+{
+	if (node == NULL)
+	{
+		*new = binary_tree_node(parent, value);
+		if (*new == NULL)
+			return (NULL);
+		return (*new);
+	}
+	if (value < node->n)
+		node->left = _bst_insert(node->left, value, node, new);
+	else if (value > node->n)
+		node->right = _bst_insert(node->right, value, node, new);
+	return (node);
+}
+
+/**
  * bst_insert - inserts a value in a binary search tree
  * @tree: is a double pointer to the root node of the BST
  * @value: is the value to store in the node to be inserted
@@ -9,40 +34,10 @@
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
-	bst_t *pivot, *newNode;
+	bst_t *newNode = NULL;
 
-	if (tree != NULL)
-	{
-		pivot = *tree;
-
-		if (pivot == NULL)
-		{
-			newNode = binary_tree_node(pivot, value);
-			if (newNode == NULL)
-				return (NULL);
-			return (*tree = newNode);
-		}
-
-		if (value < pivot->n)
-		{
-			if (pivot->left != NULL)
-				return (bst_insert(&pivot->left, value));
-
-			newNode = binary_tree_node(pivot, value);
-			if (newNode == NULL)
-				return (NULL);
-			return (pivot->left = newNode);
-		}
-		if (value > pivot->n)
-		{
-			if (pivot->right != NULL)
-				return (bst_insert(&pivot->right, value));
-
-			newNode = binary_tree_node(pivot, value);
-			if (newNode == NULL)
-				return (NULL);
-			return (pivot->right = newNode);
-		}
-	}
-	return (NULL);
+	if (tree == NULL)
+		return (NULL);
+	*tree = _bst_insert(*tree, value, *tree, &newNode);
+	return (newNode);
 }
